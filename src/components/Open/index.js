d276306ecc5cd73edf1cbddc96ebdeb8e2256c8a
Mauricio Hernan Cabrera <mauriciohernancabrera@gmail.com>
2020-06-19 12:12:58 -0300

    fix open

--- src/components/Open/index.js
+++ src/components/Open/index.js
@@ -24,6 +24,8 @@ const Open = () => {
         );
         const data = resBalance.data.results;
 
+        console.log("data", { data });
+
         if (typeof data.value_open == "number") {
           setInitialValues({
             ...data,
